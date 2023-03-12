9d1028e2b58d38e03aa4ecd946931de464326a54
raunak gurud <raunakgurud2121@gmail.com>
2023-03-12 17:03:20 +0530

    fix: deploy try 1

--- components/ui/Cards/signup.tsx
+++ components/ui/Cards/signup.tsx
@@ -29,11 +29,11 @@ export default function SignUp() {
         initialValues={{ email: '', password: '' }}
         validate={(values) => {
           const errors = {}
-          if (!values.email) {
-            errors.email = 'Required'
-          } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-            errors.email = 'Invalid email address'
-          }
+          // if (!values.email) {
+          //   errors.email = 'Required'
+          // } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+          //   errors.email = 'Invalid email address'
+          // }
           return errors
         }}
         onSubmit={(values, { setSubmitting }) => {
--- lib/apiClient.ts
+++ lib/apiClient.ts
@@ -1,7 +1,9 @@
 import axios from 'axios'
 import { parseCookies } from 'nookies'
+import { API_URL } from '../constants'
 
-const API_URL = 'http://localhost:5000/api/v1'
+// const API_URL = 'http://localhost:5000/api/v1'
+// const API_URL
 
 const instance = axios.create({
   baseURL: API_URL,
